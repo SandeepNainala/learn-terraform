68648a3a3b379c3246d29767a92cb089ccdb5289
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-26 17:01:25 +0530

    Terrafrom - data source

--- 01-vars/02-data-source/main.tf
+++ 01-vars/02-data-source/main.tf
@@ -3,6 +3,6 @@ data "aws_security_group" "selected" {
   name = "allow_all"
 }
 
-output "security_group" {
-  value = data.aws_security_group.selected
+output "security_group_id" {
+  value = data.aws_security_group.selected.id
 }
