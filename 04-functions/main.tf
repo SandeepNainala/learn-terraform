f48a0482e74bfa912d226f8f203e8968533d49d7
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-05-09 12:56:19 +0530

    terraform functions

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -1,5 +1,6 @@
+## Convert lower case to upper case
 variable "sample" {
-  default = "abc1234"
+  default = "abcxyz"
 }
 
 output "sample" {
@@ -7,20 +8,22 @@ output "sample" {
 }
 
 variable "sample1" {
-  default = ["abc","1234"]
-}
-
-output "sample1" {
-  value = element(var.sample1,0)
+  default = ["abc", "xyz"]
 }
 
 variable "sample2" {
   default = {
-      abc = 100
-      xyz = 200
+    xyz = 100
+    abc = 200
   }
 }
 
+## Print first value like var.sample1[0] with a function
+
+output "sample1" {
+  value = element(var.sample1, 0)
+}
+
 output "sample2" {
-  value = lookup(var.sample2,"a1","dummy")
+  value = lookup(var.sample2, "a1", "dummy")
 }
